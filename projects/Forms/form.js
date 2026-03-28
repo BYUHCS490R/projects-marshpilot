a86ee4314459673b0f5e233f7b48f11d17c7fa71
marshpilot <marshjoet682@byuh.edu>
2026-03-27 20:03:58 -1000

    done

--- projects/Forms/form.js
+++ projects/Forms/form.js
@@ -27,7 +27,7 @@ document.getElementById('myform').addEventListener('submit', function(event) {
     };
 
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "submit.json", true);
+    xhr.open("GET", "submit.json", true);
     xhr.setRequestHeader("Context-Type", "application/json;charset=UTF-8");
     xhr.onreadystatechange = function(){
         if (xhr.readyState === 4 && xhr.status === 200){
